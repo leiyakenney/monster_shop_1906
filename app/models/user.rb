e3b54e3a2d616a609eaa94984ddcfe6051fd3ae6
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-15 16:45:38 -0600

    Adds has_many association w/addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   has_many :item_orders
   has_many :orders, through: :item_orders
+  has_many :addresses
   belongs_to :merchant, optional: true
 
   enum role: [:regular_user, :merchant_employee, :merchant_admin, :admin_user]
