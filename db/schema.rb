41a44a2da9f9935e5765349742d3bc0d1e7dc485
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-15 16:46:21 -0600

    Adds addresses table + users/addresses association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190909231240) do
+ActiveRecord::Schema.define(version: 20190915224144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name"
+    t.string "CreateAddresses"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.string "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "item_orders", force: :cascade do |t|
     t.bigint "order_id"
@@ -87,6 +101,7 @@ ActiveRecord::Schema.define(version: 20190909231240) do
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "item_orders", "items"
   add_foreign_key "item_orders", "orders"
   add_foreign_key "item_orders", "users"
