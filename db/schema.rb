b99dd15aa8584ecf23d09ba6b88785f5cfda3ba3
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-16 17:09:57 -0600

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190915224144) do
+ActiveRecord::Schema.define(version: 20190916224532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
-    t.string "CreateAddresses"
     t.string "address"
     t.string "city"
     t.string "state"
@@ -88,10 +87,6 @@ ActiveRecord::Schema.define(version: 20190915224144) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.string "zipcode"
     t.string "email"
     t.string "password_digest"
     t.integer "role", default: 0
