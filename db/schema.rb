09ad7e96b8176e31771e76e0a7cab77e7d0faf2b
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-18 07:30:52 -0600

    Adds address_id to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190918095955) do
+ActiveRecord::Schema.define(version: 20190918130321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20190918095955) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 1
+    t.bigint "address_id"
+    t.index ["address_id"], name: "index_orders_on_address_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -97,6 +99,7 @@ ActiveRecord::Schema.define(version: 20190918095955) do
   add_foreign_key "item_orders", "orders"
   add_foreign_key "item_orders", "users"
   add_foreign_key "items", "merchants"
+  add_foreign_key "orders", "addresses"
   add_foreign_key "reviews", "items"
   add_foreign_key "users", "merchants"
 end
