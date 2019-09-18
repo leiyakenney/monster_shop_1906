822fa09409114f087affadb694b01c1172b207b1
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-18 04:13:48 -0600

    Adds ReSTFul routes for addresses nested under users

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,10 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
 
+  resources :users, only [] do
+    resources :addresses
+  end
+
   get "/profile", to: "users#show"
   get "/profile/edit", to: "users#edit"
   patch "/profile", to: "users#update"
