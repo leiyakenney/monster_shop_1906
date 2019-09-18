c83f446905aa60ff19471b2bc113d351230468bb
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-18 14:05:15 -0600

    Update flash error, updates addresses and users

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -19,23 +19,25 @@ class AddressesController< ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
+    @user = @address.user
   end
 
   def update
-    @user = current_user
-    @address = @user.addresses.where(address: @user.addresses.pluck(:address)).first
+    @address = Address.find(params[:id])
+    @user = @address.user
     if @address.shipped_orders.empty? && @address.update(address_params)
       flash[:success] = "Your address has been updated!"
       redirect_to profile_path
     else
-      flash[:error] = @user.errors.full_messages.to_sentence
+      flash[:error] = "Your address has not been updated."
       render :edit
     end
   end
 
   def destroy
-    @user = current_user
-    @address = @user.addresses.where(address: @user.addresses.pluck(:address)).first
+    @address = Address.find(params[:id])
+    @user = @address.user
     if @address.shipped_orders.empty?
       @address.destroy
       flash[:success] = "Your address has been deleted!"
