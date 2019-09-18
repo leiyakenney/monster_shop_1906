ce7e23ee8bb70cdb31c889f1abafb67da1d3fda9
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-18 07:32:33 -0600

    Updates address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,12 @@
 class Address < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip, :nickname
+  validates_presence_of :name, :address, :city, :state, :zip
+  validates :nickname, presence: true, uniqueness: { scope: :user_id}
 
   belongs_to :user
   has_many :orders
+
+  def shipped_orders
+    orders.where(status: 'shipped')
+  end
 
 end
