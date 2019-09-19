4959ab6ea6196cf9d70cdf700804e73233adc2d6
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-19 09:24:21 -0600

    Adds post route

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
   get "/profile/orders", to: "users#show_orders"
   post "/profile/orders", to: "orders#create"
   get "/profile/orders/new", to: "orders#new"
+  post "/profile/orders/new", to: "orders#create"
   get "/profile/orders/:id", to: "users#show_order"
 
   post "/cart/:item_id", to: "cart#add_item"
